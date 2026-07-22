02af34727bc7a23b93003350267fc4d5225bcbde
Wojciech Gawroński <wojtek.gawronski@getunleash.io>
2026-07-22 13:51:39 +0200

    chore: Modernizing ESlint setup

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default tseslint.config(
+  { ignores: ['dist', 'backend/dist'] },
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended
+    ],
+    languageOptions: {
+      globals: globals.browser
+    },
+    plugins: {
+      'react-refresh': reactRefresh
+    },
+    rules: {
+      'react-refresh/only-export-components': 'warn'
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error'
+    }
+  }
+)
